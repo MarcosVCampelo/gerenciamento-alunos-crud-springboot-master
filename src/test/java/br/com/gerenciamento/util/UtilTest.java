6e091a9d40be37a6cfcec071418cec9fc64dec68
TGianone <thigianone@hotmail.com>
2023-11-20 17:41:27 -0300

    testes util concluidos

--- src/test/java/br/com/gerenciamento/util/UtilTest.java
+++ src/test/java/br/com/gerenciamento/util/UtilTest.java
@@ -1,5 +1,36 @@
 package br.com.gerenciamento.util;
 
-public class UtilTest {
-    
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UtilTest {
+
+    @Test // hash MD5 = "senha123"
+    void testMd5() throws NoSuchAlgorithmException {
+        String senha = "senha123";
+        String expectedHash = "e7d80ffeefa212b7c5c55700e4f7193e";
+
+        String result = Util.md5(senha);
+
+        assertEquals(expectedHash, result);
+    }
+
+    @Test // hash MD5 = vazio
+    void testMd5EmptyString() throws NoSuchAlgorithmException {
+        String senha = "";
+        String expectedHash = "d41d8cd98f00b204e9800998ecf8427e";
+
+        String result = Util.md5(senha);
+
+        assertEquals(expectedHash, result);
+    }
+
+    @Test //valida entrada nula
+    void testMd5NullString() {
+        assertThrows(NullPointerException.class, () -> Util.md5(null));
+    }
 }
