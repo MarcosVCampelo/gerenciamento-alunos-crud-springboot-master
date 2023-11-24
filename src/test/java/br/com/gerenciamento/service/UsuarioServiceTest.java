b689e5707ea72bcc807e7945717c73f3ab295ae8
Mohammed Bader <mohmdbader217@outlook.com>
2023-11-24 10:15:45 -0300

    UsuarioServiceTest done

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,15 +1,20 @@
 package br.com.gerenciamento.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 import java.security.NoSuchAlgorithmException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.gerenciamento.exception.ServiceExc;
 import br.com.gerenciamento.model.Usuario;
 import br.com.gerenciamento.repository.UsuarioRepository;
 
@@ -39,5 +44,28 @@ public class UsuarioServiceTest {
         // Assert
         verify(usuarioRepository, times(1)).findByEmail(eq("test@example.com"));
         verify(usuarioRepository, times(1)).save(any(Usuario.class));
+    }
+    @Test
+    public void testLoginUser() throws ServiceExc {
+        // Dados de exemplo
+        String usuario = "username";
+        String senha = "password";
+
+        // Criando um usuário simulado para retorno
+        Usuario usuarioSimulado = new Usuario();
+        usuarioSimulado.setUser(usuario);
+        usuarioSimulado.setSenha(senha);
+
+        // Definindo o comportamento simulado para o método buscarLogin no repositório
+        when(usuarioRepository.buscarLogin(usuario, senha)).thenReturn(usuarioSimulado);
+
+        // Chamando o método a ser testado
+        Usuario resultado = usuarioService.loginUser(usuario, senha);
+
+        // Verificando se o método de retorno é o esperado
+        assertEquals(usuarioSimulado, resultado);
+
+        // Verificando se o método buscarLogin foi chamado com os parâmetros corretos
+        verify(usuarioRepository, times(1)).buscarLogin(usuario, senha);
     }
 }
